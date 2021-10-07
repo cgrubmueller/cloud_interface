c67fb94a77b9856c5bc7398f55ab1f70fe44d854
Christian Grubmüller <cgrubmueller@student.tgm.ac.at>
2021-10-07 14:48:44 +0200

    register implementiert

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+curl -X POST -F 'bname=linuxize' -F 'email=linuxize@example.com' -F 'passwd=12345' http://127.0.0.1:8001/signup
+```
+
--- src/rest.py
+++ src/rest.py
@@ -4,41 +4,16 @@ import time
 import json
 import urllib3
 import sqlite3 as sql
+import hashlib
 
 app = Flask(__name__)
 
 
 allData = ""
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sql.connect('database.db')
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
-@app.route("/login", methods=["GET"])
-def loginGET():
-    return '''
-        <form action="/loginPost">
-          <label for="bname">Benutzername:</label>
-          <input type="text" id="bname" name="bname"><br><br>
-          <label for="email">E-Mail:</label>
-          <input type="email" id="email" name="email"><br><br>
-          <label for="passwd">Passwort:</label>
-          <input type="password" id="passwd" name="passwd"><br><br>
-          <input type="submit" value="Submit">
-        </form>
-    '''
 
 @app.route("/loginPost", methods=["POST"])
 def loginPost():
@@ -47,13 +22,16 @@ def loginPost():
         email = request.form['email']
         passwd = request.form['passwd']
 
+        print("blaskfa")
+        print(bname)
+        print(email)
+        print(passwd)
+
         with sql.connect("database.db") as con:
             cur = con.cursor()
 
-            cur.execute('CREATE TABLE IF NOT EXISTS user (email TEXT PRIMARY KEY, name TEXT, passwd TEXT)')
-
-            cur.execute('''INSERT INTO benutzer (bname,email,passwd)
-            VALUES (?,?,?),(bname,email,passwd)''')
+            cur.execute('''INSERT INTO benutzer (bname,email,md5(passwd))
+            VALUES (?,?,?)''',(bname,email,passwd))
 
             con.commit()
             msg = "Record successfully added"
@@ -65,13 +43,28 @@ def loginPost():
         return msg
         con.close()
 
-@app.route("/signup", methods=["POST"])
+@app.route("/register", methods=["POST"])
 def signupPOST():
-    return
+    try:
+        bname = request.form['bname']
+        email = request.form['email']
+        passwd = request.form['passwd']
 
-@app.route("/signup", methods=["GET"])
-def signupGET():
-    return
+        with sql.connect("database.db") as con:
+            # con.execute("drop table benutzer")
+            con.execute('CREATE TABLE IF NOT EXISTS benutzer (email TEXT PRIMARY KEY, bname TEXT, passwd TEXT)')
+
+            con.execute('''INSERT INTO benutzer (bname,email,passwd) VALUES (?,?,?)''', (bname, email,passwd))
+
+            con.commit()
+            msg = f"user {bname} successfully added to database"
+
+    except:
+        con.rollback()
+        msg = "error in insert operation"
+    finally:
+        return msg
+        con.close()
 
 
 if __name__ == "__main__":
