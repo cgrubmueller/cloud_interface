eaf61b48811ecde119b5a9666994ddf22ad2262f
cgrubmueller <chris.grubmueller@gmail.com>
2021-12-11 11:06:18 +0100

    .gitignore geupdatet;salts hinzugefuegt;https hinzugefuegt?

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@ FROM python:3.7-alpine
 
 RUN mkdir /app
 WORKDIR /app
+ADD gunicorn.sh /app
 ADD requirements.txt /app
 ADD src/rest.py /app
 ADD src/database.db /app
 
 RUN pip3 install -r requirements.txt
 
-EXPOSE 8001
-CMD ["python3", "rest.py"]
+ENTRYPOINT ["./gunicorn.sh"]
+#EXPOSE 8001
+#CMD ["python3", "rest.py"]
--- src/rest.py
+++ src/rest.py
@@ -1,9 +1,24 @@
+import hashlib
+import traceback
+import uuid
+
 from flask import Flask, request
 import sqlite3 as sql
 import re
 
 # Regex-Pattern für E-Mail
 email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+# Hasht ein Passwort und generiert einen salt-Wert
+def hash_password(password):
+    # uuid is used to generate a random number
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+
+# Überprüft, ob ein Passwort richtig ist.
+def check_password(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
 
 app = Flask(__name__)
 
@@ -24,20 +39,25 @@ def loginPost():
             # connecting to the database
             with sql.connect("database.db") as con:
                 cur = con.cursor()
-                cur.execute("SELECT bname, email, passwd FROM benutzer WHERE email = ? AND passwd = ?", (email, hash(passwd)))
+                cur.execute(f"SELECT bname, email, passwd FROM benutzer WHERE email = ?", [email])
                 users = cur.fetchall()
+
                 if (len(users) == 1):
-                    msg = "\nWelcome!"
+                    if check_password(users[0][2] , passwd):
+                        msg = "\nWelcome!"
+                    else:
+                        msg = "\nUsername or password was not correct!"
                 elif (len(users) > 1):
                     msg = "\nInternal error: multiple users with the same email???"
                 else:
-                    msg = "\nUsername or password was not correct!"
+                    msg = "\nThere are no user registered with this email!"
         else:
             msg= f"\nThe email {email} is invalid. Try again with a valid email."
 
     except:
+        traceback.print_exc()
         con.rollback()
-        msg = "\nAn error occured while logging you in!"
+        msg = "\nNo user is registered!"
     finally:
         return msg
         con.close()
@@ -56,7 +76,7 @@ def signupPOST():
             with sql.connect("database.db") as con:
                 con.execute("CREATE TABLE IF NOT EXISTS benutzer (email TEXT PRIMARY KEY, bname TEXT, passwd TEXT)")
 
-                con.execute("INSERT INTO benutzer (bname,email,passwd) VALUES (?,?,?)", (bname, email,hash(passwd)))
+                con.execute("INSERT INTO benutzer (bname,email,passwd) VALUES (?,?,?)", (bname, email,hash_password(passwd)))
 
                 con.commit()
                 msg = f"\nUser {bname} successfully added to database."
@@ -65,7 +85,7 @@ def signupPOST():
 
     except:
         con.rollback()
-        msg = "\nAn user with this email is already registered!"
+        msg = "\nThis email is already in use!"
     finally:
         return msg
         con.close()
@@ -85,4 +105,4 @@ def reset():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8001)
+    app.run(debug=True, host='0.0.0.0', port=8001, ssl_context='adhoc')
